cadef1472355b6403e7f49387a3603acef52316f
James Hatfield <jahatfi@gmail.com>
2024-05-08 23:05:41 -0400

    Linted test files

--- tests/example_procedural_division/divide_ints.py
+++ tests/example_procedural_division/divide_ints.py
@@ -1,3 +1,7 @@
+"""
+Trivial example to demonstrate DDT concept
+"""
+
 import argparse
 import logging
 import os
@@ -10,18 +14,22 @@ from src.unit_test_generator import (
     unit_test_generator_decorator,
 )
 
-fmt_str = '%(levelname)-8s|%(module)-16s|%(funcName)-20s:%(lineno)-4d:%(message)s'
-logging.basicConfig(level=logging.INFO, format=fmt_str)
+FMT_STR = '%(levelname)-8s|%(module)-16s|%(funcName)-20s:%(lineno)-4d:%(message)s'
+logging.basicConfig(level=logging.INFO, format=FMT_STR)
 logger = logging.getLogger(__name__)
 unit_test_generator.logger.setLevel(logging.CRITICAL)
 
 # Use a global variable to test that unit_test_generator_decorator
 # considers global variables when generating unit tests
-error_code = 0
+error_code = 0 # pylint: disable=invalid-name
 
 def divide_ints(a: int, b: int):
-    global error_code
-    logger.info(f"{error_code=}")
+    """
+    Divide two numbers, raise TypeError or ValueError if not ints 
+    or denominator is 0, respectively.
+    """
+    global error_code # pylint: disable=global-statement
+    logger.info("error_code=%d", error_code)
     if not isinstance(a, int):
         error_code = -1
         raise TypeError(f"TypeError: Variable {a=} is not an int!")
@@ -34,19 +42,25 @@ def divide_ints(a: int, b: int):
     return f"{a}/{b}={a/b}"
 
 def main():
+    """
+    Call division function
+    """
     start = time.perf_counter()
     a_list = [6, 3,"10", 8, [], {}, 4]
     b_list = [2, 0, 2, [], 2, 3, set()]
-
+    global error_code  # pylint: disable=global-statement
     for a, b in zip(a_list, b_list):
         try:
             logger.info("-"*80)
             error_code = 0
-            logger.info(f"Trying divide_ints({a}, {b})")
-            logger.info(f"divide_ints({a}, {b})={divide_ints(a, b)}")
-        except Exception as e:
-            logger.error(f"{type(e)}: {e}")
-        logger.info(f"{error_code=}")
+            try:
+                logger.info("Trying divide_ints(%s, %s)", repr(a), repr(b))
+            except TypeError as e:
+                logger.info(e)
+            print(f"divide_ints({a}, {b})={divide_ints(a, b)}")
+        except (ValueError, TypeError) as e:
+            logger.error("%s: %s", type(e),str(e))
+        logger.info("error_code=%d", error_code)
 
     generate_all_tests_and_metadata(Path('.'), Path('.'))
     print(f"Took {time.perf_counter()-start} seconds")
