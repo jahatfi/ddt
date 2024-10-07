69b6df805609838cbce53ee17e90f91148f9bb23
James Hatfield <jahatfi@gmail.com>
2024-10-06 22:56:10 -0400

    Sorted input

--- tests/fizzbuzz/test_coverage_str_helper.py
+++ tests/fizzbuzz/test_coverage_str_helper.py
@@ -9,25 +9,25 @@ from src import unit_test_generator
 
 # Now import modules specific to coverage_str_helper:
 from logging import RootLogger
-from logging import StreamHandler
 from logging import PlaceHolder
+from logging import StreamHandler
 from logging import Logger
 from logging import Manager
 
 
 # In sum, these tests covered 71.88% of coverage_str_helper's lines
 # Line(s) not covered by ANY of the tests below:
-# ['1758', '1769', '1774-1775', '1783-1784', '1787-1788']
+# ['1743', '1754', '1759-1760', '1768-1769', '1772-1773']
 @pytest.mark.parametrize(
     "this_list, non_code_lines, expected_result, args_after",
     [
         (
-            [1698, 1699, 1701, 1702, 1703, 1715, 1716],
-            {1704, 1706, 1707, 1708, 1709, 1710, 1711, 1712},
-            "['1698-1699', '1701-1703', '1715-1716']",
+            [1701, 1702, 1684, 1685, 1687, 1688, 1689],
+            {1696, 1697, 1698, 1690, 1692, 1693, 1694, 1695},
+            "['1701-1702', '1684-1685', '1687-1689']",
             {
-                "this_list": "[1698, 1699, 1701, 1702, 1703, 1715, 1716]",
-                "non_code_lines": "{1704, 1706, 1707, 1708, 1709, 1710, 1711, 1712}",
+                "this_list": "[1701, 1702, 1684, 1685, 1687, 1688, 1689]",
+                "non_code_lines": "{1696, 1697, 1698, 1690, 1692, 1693, 1694, 1695}",
             },
         ),
     ],
--- tests/fizzbuzz/test_update_global.py
+++ tests/fizzbuzz/test_update_global.py
@@ -8,17 +8,17 @@ from collections import OrderedDict
 from src import unit_test_generator
 
 # Now import modules specific to update_global:
-from logging import RootLogger
 from src.unit_test_generator import CoverageInfo
-from logging import StreamHandler
 from logging import PlaceHolder
+from logging import StreamHandler
 from logging import Logger
+from logging import RootLogger
 from logging import Manager
 
 
 # In sum, these tests covered 46.15% of update_global's lines
 # Line(s) not covered by ANY of the tests below:
-# ['1698-1699', '1701-1703', '1715-1716']
+# ['1701-1702', '1684-1685', '1687-1689']
 @pytest.mark.parametrize(
     "obj, this_global, phase, this_coverage_info, expected_result, args_after",
     [
@@ -44,10 +44,7 @@ from logging import Manager
             ),
             "CoverageInfo(args_before=['6'], args_after={}, kwargs={}, kwargs_after={}, globals_before={'mode': 'fizzbuzz'}, globals_after={}, expected_result='', expected_type='', coverage=[], exception_type='', exception_message='', constructor='', cost=0.0, testable=0.0)",
             {
-                "obj": '"fizzbuzz"',
-                "this_global": '"mode"',
-                "phase": '"Before"',
-                "this_coverage_info": "CoverageInfo(args_before=['6'], args_after={}, kwargs={}, kwargs_after={}, globals_before={'mode': 'fizzbuzz'}, globals_after={}, expected_result='', expected_type='', coverage=[], exception_type='', exception_message='', constructor='', cost=0.0, testable=0.0)",
+                "this_coverage_info": "CoverageInfo(args_before=['6'], args_after={}, kwargs={}, kwargs_after={}, globals_before={'mode': 'fizzbuzz'}, globals_after={}, expected_result='', expected_type='', coverage=[], exception_type='', exception_message='', constructor='', cost=0.0, testable=0.0)"
             },
         ),
     ],
@@ -62,12 +59,6 @@ def test_update_global(
         obj, this_global, phase, this_coverage_info
     )
     assert result == expected_result or result == eval(expected_result)
-    assert obj == eval(args_after["obj"]) or args_after["obj"] == obj
-    assert (
-        this_global == eval(args_after["this_global"])
-        or args_after["this_global"] == this_global
-    )
-    assert phase == eval(args_after["phase"]) or args_after["phase"] == phase
     assert (
         this_coverage_info == eval(args_after["this_coverage_info"])
         or args_after["this_coverage_info"] == this_coverage_info
