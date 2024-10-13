0dacf3a76900be9f399d20c3c94c3cb7560ffdb1
James <jahatfi@gmail.com>
2024-10-13 10:15:39 -0400

    Update test_get_module_import_string.py
    OS agnostic path

--- src/test_get_module_import_string.py
+++ src/test_get_module_import_string.py
@@ -6,6 +6,7 @@ import re
 import pytest
 from collections import OrderedDict
 from src import unit_test_generator
+import pathlib
 
 # Now import modules specific to get_module_import_string:
 from logging import Manager
@@ -13,7 +14,7 @@ from logging import PlaceHolder
 from logging import StreamHandler
 from logging import RootLogger
 from logging import Logger
-from pathlib import WindowsPath
+#from pathlib import WindowsPath
 
 
 # In sum, these tests covered 85.0% of get_module_import_string's lines
@@ -23,12 +24,10 @@ from pathlib import WindowsPath
     "my_path, expected_result, args_after",
     [
         (
-            WindowsPath(
-                "ddt/tests/oo_car/car.py"
-            ),
+            Path("ddt/tests/oo_car/car.py").absolute(),
             "tests.oo_car.car",
             {
-                "my_path": "WindowsPath('ddt/tests/oo_car/car.py')"
+                "my_path": "Path('ddt/tests/oo_car/car.py').absolute()"
             },
         ),
     ],
