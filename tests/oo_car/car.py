e1714e862dd55c747df0b3dfd25db47ef36749ea
James Hatfield <jahatfi@gmail.com>
2025-11-13 18:29:55 -0500

    sleep 1

--- tests/oo_car/car.py
+++ tests/oo_car/car.py
@@ -247,8 +247,9 @@ if __name__ == "__main__":
         if file.suffix in (".py", ".json") and file.absolute() != this_file:
             logger.debug("%s != %s", file.absolute().name, this_file.name)
             logger.debug("Deleting %s to ensure clean start", file.name)
-            print(f"Removing {file}")
+            logger.warning(f"Removing {file=}")
             os.remove(file)
+    logger.warning("Sleep 1 s...")
     time.sleep(1)
     if args.disable_unit_test_generation:
         main()
